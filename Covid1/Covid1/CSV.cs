d4b6b7bdca6c282b98bc172be8497384fb93bb56
Miconaut <71273636+Miconaut@users.noreply.github.com>
2020-10-28 15:50:56 +0100

    Add files via upload

---
+++ Covid1/Covid1/CSV.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Covid1
+{
+    class CSV
+    {
+        public const int CSV_SCHEMA_LINE_INDEX = 0;
+
+        public List<CSVList> Data = new List<CSVList>();
+
+        public CSV(string filePath)
+        {
+            string[] Lines = File.ReadAllLines(filePath);
+
+            string[] Schema = GetCSVLineData(Lines[CSV_SCHEMA_LINE_INDEX]);
+
+            foreach (string ElementName in Schema)
+            {
+                Data.Add(new CSVList(ElementName));
+            }
+
+            for (int Line = CSV_SCHEMA_LINE_INDEX + 1; Line < Lines.Length; Line++)
+            {
+                string[] LineData = GetCSVLineData(Lines[Line]);
+
+                for (int DataIdx = 0; DataIdx < Data.Count; DataIdx++)
+                {
+                    Data[DataIdx].Values.Add(LineData[DataIdx]);
+                }
+            }
+        }
+
+        public CSVList GetListForName(string Name)
+        {
+            foreach (CSVList List in Data){
+                if (List.Name == Name)
+                {
+                    return List;
+                }
+            }
+            return null;
+        }
+
+        public static string[] GetCSVLineData(String line)
+        {
+            return line.Split(',');
+        }
+    }
+
+    class CSVList
+    {
+        public string Name;
+
+        public List<string> Values = new List<string>();
+
+        public CSVList(string Name)
+        {
+            this.Name = Name;
+        }
+
+        public int GetSize()
+        {
+            return Values.Count;
+        }
+
+        public List<string> GetStrings()
+        {
+            return Values;
+        }
+
+        public List<double> GetDoubleFloats()
+        {
+            List<double> RetnList = new List<double>();
+            foreach (string str in Values)
+            {
+                RetnList.Add(Double.Parse(str));
+            }
+            return RetnList;
+        }        
+    }
+}
---
+++ Covid1/Covid1/Form1.cs
@@ -0,0 +1,58 @@
+﻿using LiveCharts.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Covid1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private const string COVID1_CSV_PATH = "Covid1.csv";
+        private const string WORLD_XML_PATH = "World.xml";
+
+        [Obsolete]
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var r = new Random();
+            LiveCharts.WinForms.GeoMap geoMap = new LiveCharts.WinForms.GeoMap();
+            Dictionary<string, double> keyValues = new Dictionary<string, double>();
+
+            CSV csv = new CSV(COVID1_CSV_PATH);
+
+            List<string> Znacka = csv.GetListForName("Znacka").GetStrings();
+            List<double> Corona = csv.GetListForName("Corona").GetDoubleFloats();
+
+            for (int Index = 0; Index < Znacka.Count; Index++)
+            {
+                keyValues.Add(Znacka[Index], Corona[Index]);
+            }
+
+            geoMap.HeatMap = keyValues;
+
+            geoMap.Source = WORLD_XML_PATH;
+            this.Controls.Add(geoMap);
+            geoMap.Dock = DockStyle.Fill;
+
+            geoMap.LandClick += GeoMap_LandClick;
+            geoMap.EnableZoomingAndPanning = true;
+            geoMap.Hoverable = true;
+        }
+        private void GeoMap_LandClick(object arg1, LiveCharts.Maps.MapData arg2)
+        {
+            Console.WriteLine(arg2.Id);
+        }
+    }
+}
